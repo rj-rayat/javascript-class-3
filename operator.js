8863fbd207fefd918a9a7a63afae78b5262f252f
rj-rayat <rayatrj36@gmail.com>
2026-03-02 14:36:16 +0600

    first commit

---
+++ operator.js
@@ -0,0 +1,125 @@
+const apple = 12
+const komola = 10
+const totalFol = apple + komola
+console.log("koyta fol", totalFol )
+
+
+// console.log(arBakiThakeKoto)
+
+const rayhan = 5 
+const chereGeceKoita = 3
+const bakiJon = rayhan - chereGeceKoita
+console.log("Afsos : ", bakiJon)
+
+// Multiplication 
+
+const manushAche =  13
+const iPhone = 3
+const totalIphoneNeed = manushAche * iPhone
+
+
+// console.log(totalIphoneNeed) 
+
+// Division
+
+const iPhoneAche = 40
+const kotojonManush = 10
+const pabeKoita = iPhoneAche / kotojonManush
+console.log(pabeKoita)
+
+
+// Modulus
+
+const amarBostaAche = 33
+const vagKoreDilam = 2
+const vagSes = amarBostaAche % vagKoreDilam
+console.log(vagSes)
+
+
+
+
+// Greater Than >
+// less than <
+// equal to ==
+// greater than >=
+// less than <= 
+// not equal to !=
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ script.js
@@ -0,0 +1,30 @@
+// addition +
+// substraction -
+// multiplication *
+// division / 
+// modulus % 
+
+
+// console.log("jog", 4657847+29525957)
+//4657847529525957
+// console.log("biyog", 3095858-155857)
+// console.log("gun", 6*77)
+// console.log("vag", 6 / 2)
+
+// // console.log("vag ses", 5 % 2)
+
+
+// // const rayat = "45" //string 
+// // const sonkha = 45
+
+// // // amra jokhn "rayat"
+
+// const amiKiGonitPari = true //yes
+// const amiGonitParina = false // no
+
+// const name ="jslkdalj" //string
+// const sonkha = 34 //number // integer
+// const amiacikinai = true // boolean
+// const aminai = false // boolean
+
+
